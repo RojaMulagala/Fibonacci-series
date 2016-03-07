d098cda64920ddc40600fa5c792918825d75331e
Roja <rojaramani@orangescape.com>
2016-03-07 15:17:57 +0530

    dynamic fib series

--- fib.py
+++ fib.py
@@ -1,11 +1,9 @@
 def fib(n):
     if(n<=1):
-	    return n
+        return n
     else:
-	    return fib(n-1)+fib(n-2)
-
-
-p=int(input("enter the number")
+        return fib(n-1)+fib(n-2)
+p=int(input("enter the number"))
 for i in range(p):
     print fib(i)
 
